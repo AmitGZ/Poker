a6d6acf3ffd00bf72a4fe3c982d71df6af142ec1
OfekBenAtar <benatarofek@gmail.com>
2022-04-18 23:54:27 +0300

    Update MyHashTable class

--- Poker/ClassLibrary/MyHashTable.cs
+++ Poker/ClassLibrary/MyHashTable.cs
@@ -1,31 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Poker.ClassLibrary
 {
     public class MyHashTable<T>
     {
-        private struct item<Struct_T>
-        {
-            public int key;
-            public T value;
-        }
-
-        private List<item<T>?> table;
+        private List<T> table;
 
         public MyHashTable(int number_of_items)
         {
-            table = new List<item<T>?>();
+            table = new List<T>();
             for(int i = 0; i < number_of_items; i++)
-                table.Add(null);
+                table.Add(default); // default is null in external classes
         }
 
-        public void Add(int index,T value)
-        { 
-            // TODO : add a new player into the hash table
+        private bool isEmpty()
+        {
+            // True : all items arr null.
+            // False: there is NOT null item.
+            return table.Where(t => t != null).Count() == 0;
+        }
+        private bool isFull()
+        {
+            // True : number_of_items from the Builder is equal to
+            //        the number of Not null elements. Can't find null element.
+            // False: there is at least 1 null element in the Hash Table.
+            return (table.Count() == table.Where(t => t != null).Count());
+        }
+        public bool Add(int index,T value)
+        {
+            if(isFull())
+                return false;
+            int availble_index = nextAvialbleIndex(index);
+            table[availble_index] = value;
+            return true;
+        }
+        private int nextAvialbleIndex(int index)
+        {
+            // TODO : reurn the next null cell index
+            throw new NotImplementedException();
         }
 
         public void Remove(T value)
-        {
+        { 
             // TODO : remove a new player from the hash table
         }
 
