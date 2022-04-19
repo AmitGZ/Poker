7e1f08b13a365adfc6b16034e9862d59f90a656c
OfekBenAtar <benatarofek@gmail.com>
2022-04-19 03:19:54 +0300

    Finish MyHashTable class and use it in GameEngine

--- Poker/ClassLibrary/MyHashTable.cs
+++ Poker/ClassLibrary/MyHashTable.cs
@@ -4,51 +4,58 @@ using System.Linq;
 
 namespace Poker.ClassLibrary
 {
-    public class MyHashTable<T>
+    public class MyHashTable
     {
-        private List<T> table;
-
+        private List<Player> table_members;
         public MyHashTable(int number_of_items)
         {
-            table = new List<T>();
+            table_members = new List<Player>();
             for(int i = 0; i < number_of_items; i++)
-                table.Add(default); // default is null in external classes
+                table_members.Add(null); // default is null in external classes
         }
-
         private bool isEmpty()
         {
             // True : all items arr null.
             // False: there is NOT null item.
-            return table.Where(t => t != null).Count() == 0;
+            return table_members.Where(p => p != null).Count() == 0;
         }
         private bool isFull()
         {
             // True : number_of_items from the Builder is equal to
             //        the number of Not null elements. Can't find null element.
             // False: there is at least 1 null element in the Hash Table.
-            return (table.Count() == table.Where(t => t != null).Count());
+            return (table_members.Count() == table_members.Where(p => p != null).Count());
         }
-        public bool Add(int index,T value)
+        public bool Add(int index,Player value)
         {
             if(isFull())
                 return false;
-            int availble_index = nextAvialbleIndex(index);
-            table[availble_index] = value;
+            table_members[index] = value;
             return true;
         }
-        private int nextAvialbleIndex(int index)
+        public void Remove(string player_id)
         {
-            // TODO : reurn the next null cell index
-            throw new NotImplementedException();
+            int index = table_members.FindIndex(p => p.Id == player_id);
+            table_members[index] = null;
         }
-
-        public void Remove(T value)
-        { 
-            // TODO : remove a new player from the hash table
+        public void setEveryonePlaying()
+        {
+            foreach (Player player in table_members.Where(p => p.isInGame == false))
+                player.isInGame = true;
         }
-
         public int Count()
-        { return table.Count; }
-
+        { return table_members.Count; }
+        public Player FindAdjacentPlayer(Player current_player)
+        {
+            int current_player_index = table_members.FindIndex(p => p.Id == current_player.Id);
+            int tmp_player_index = current_player_index;
+            do
+            {
+                tmp_player_index = (tmp_player_index + 1) % table_members.Count;
+                if (table_members[tmp_player_index] != null)
+                    break;
+            } while (tmp_player_index != current_player_index);
+            return table_members[tmp_player_index];
+        }
     }
 }
