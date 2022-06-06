e3c8655f121f99462cae56ff605ca6352fac0bd6
amit zohar <amitzohar4@gmail.com>
2022-06-07 01:10:02 +0300

    added workflow

--- Poker/Poker/Hub/PokerHub.cs
+++ Poker/Poker/Hub/PokerHub.cs
@@ -115,7 +115,7 @@ namespace Poker.Hubs
             if(!gameRunning)
             {
                 // Game Ended
-                await HandleEndedGame(room);
+                await SendEndedGame(room);
             }
 
             // Sending everyone in the room the status
@@ -179,7 +179,7 @@ namespace Poker.Hubs
             if (!gameRunning)
             {
                 // Game Ended
-                await HandleEndedGame(room);
+                await SendEndedGame(room);
             }
 
             SendRoomStatus(room);             // Sending everyone in the room the status
@@ -204,7 +204,7 @@ namespace Poker.Hubs
             if (!gameRunning)
             {
                 // Game ended
-                await HandleEndedGame(room);
+                await SendEndedGame(room);
             }
 
             SendRoomStatus(room);             // Sending everyone in the room the status
@@ -229,7 +229,7 @@ namespace Poker.Hubs
             if (!gameRunning)
             {
                 // Game ended
-                await HandleEndedGame (room);
+                await SendEndedGame (room);
             }
 
             SendRoomStatus(room);             // Sending everyone in the room the status
@@ -254,7 +254,7 @@ namespace Poker.Hubs
             if (!gameRunning)
             {
                 // Game ended
-                await HandleEndedGame(room);
+                await SendEndedGame(room);
             }
 
             SendRoomStatus(room);             // Sending everyone in the room the status
@@ -321,7 +321,7 @@ namespace Poker.Hubs
                 if (room.Fold(talkingUser))
                 {
                     // Game Ended
-                    await HandleEndedGame(room);
+                    await SendEndedGame(room);
                     ResetTimer(room.Id);
                 }
             }
@@ -370,7 +370,7 @@ namespace Poker.Hubs
             }
         }
 
-        private async Task HandleEndedGame(Room room)
+        private async Task SendEndedGame(Room room)
         {
             ResetTimer(room.Id);
             UserInGame winner = room.Users.FirstOrDefault(u => u.IsWinner == true);
