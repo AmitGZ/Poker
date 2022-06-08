c92ac6874ce02fb121d4efc085ab27312a3bb182
amit zohar <amitzohar4@gmail.com>
2022-06-08 20:50:03 +0300

    test with react app

--- FrontEnd/src/components/ConnectedUsers.js
+++
@@ -1,9 +0,0 @@
-const ConnectedUsers = ({ users }) => {
-    return(
-    <div className='user-list'>
-        <h4>Connected Users</h4>
-        {users.map((u, idx) => <h5 key={idx}>{u.username}</h5>)}
-    </div>);
-};
-
-export default ConnectedUsers;
